1e2a11afbd1529dd1d0b0d96365438f196ceef62
BiesterVictor <victor.biester@nordlb.de>
2025-06-24 08:52:48 +0200

    initial commit

---
+++ src/main/java/org/example/suppcheck/SuppCheckApplication.java
@@ -0,0 +1,13 @@
+package org.example.suppcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuppCheckApplication.class, args);
+    }
+
+}
